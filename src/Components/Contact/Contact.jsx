119f41debd97ef04315b2045fc93f27869441dde
Haroon Saifi <haroonsaifi2001@gmail.com>
2024-03-06 02:06:30 +0530

    GFG: Team card Added

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -10,17 +10,15 @@ function Contact() {
           <h1 className="text-[#FFB100] text-5xl font-semibold text-center">
             Get in touch!
           </h1>
-          <div className="mx-auto max-w-[450px] my-4 h-[2px] bg-white"></div>
+          <div className="mx-auto max-w-[450px] my-4 h-[2px] w-[70vw] bg-white"></div>
           <h3 className=" text-3xl font-semibold text-center">
             Contact us for any queries
           </h3>
           <p className="mt-8 text-center max-w-[600px] mx-auto">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
-            aspernatur quia soluta possimus nulla distinctio amet ab deleniti
-            doloremque reiciendis officia fugiat corrupti laboriosam maxime
-            animi tempore ad, dolor nostrum. Autem, voluptas unde.
-            Reprehenderit, eius accusamus sed, quo fugit esse alias odit
-            consequatur veniam eveniet recusandae at ducimus sequi eaque.
+            Got a question? Need assistance? Don't hesitate to reach out to us!
+            Connect with us on our social media handles for updates, events, and
+            exciting coding challenges. <br/> Join our
+            community today and let's code our way to success together!
           </p>
         </div>
       </div>
--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -13,41 +13,70 @@ function Team() {
             </h1>
           </div>
           <div className="flex gap-8 justify-center items-center flex-wrap mt-8">
-            <div>
-              <img
-                src="https://placehold.it/200"
-                className="border-4 border-white rounded-sm"
-                alt=""
-              />
-              <h4 className="text-center my-2 font-semibold text-2xl">Name</h4>
-              <h5 className="text-center text-xl">Position</h5>
+            <div className="bg-card p-8">
+              <div className="shadow1 p-1">
+                <img
+                  src="/assets/anushka.webp"
+                  className=" w-[250px] rounded-sm"
+                  alt=""
+                />
+              </div>
+              <h4 className="text-center my-2 font-semibold text-2xl">
+                Anushka Gupta
+              </h4>
+              <h5 className="text-center text-xl">President</h5>
             </div>
-            <div>
-              <img
-                src="https://placehold.it/200"
-                className="border-4 border-white rounded-sm"
-                alt=""
-              />
-              <h4 className="text-center my-2 font-semibold text-2xl">Name</h4>
-              <h5 className="text-center text-xl">Position</h5>
+            <div className="bg-card p-8">
+              <div className="shadow1 p-1">
+                <img
+                  src="/assets/satyam.webp"
+                  className=" w-[250px] rounded-sm"
+                  alt=""
+                />
+              </div>
+              <h4 className="text-center my-2 font-semibold text-2xl">
+                Satyam Saini
+              </h4>
+              <h5 className="text-center text-xl">Voice President</h5>
             </div>
-            <div>
-              <img
-                src="https://placehold.it/200"
-                className="border-4 border-white rounded-sm"
-                alt=""
-              />
-              <h4 className="text-center my-2 font-semibold text-2xl">Name</h4>
-              <h5 className="text-center text-xl">Position</h5>
+            <div className="bg-card p-8">
+              <div className="shadow1 p-1">
+                <img
+                  src="/assets/hardik.webp"
+                  className="w-[250px]  rounded-sm"
+                  alt=""
+                />
+              </div>
+              <h4 className="text-center my-2 font-semibold text-2xl">
+                Hardik Sharma
+              </h4>
+              <h5 className="text-center text-xl">General Secrectary</h5>
             </div>
-            <div>
-              <img
-                src="https://placehold.it/200"
-                className="border-4 border-white rounded-sm"
-                alt=""
-              />
-              <h4 className="text-center my-2 font-semibold text-2xl">Name</h4>
-              <h5 className="text-center text-xl">Position</h5>
+            <div className="bg-card p-8">
+              <div className="shadow1 p-1">
+                <img
+                  src="/assets/mansi.webp"
+                  className="w-[250px] rounded-sm"
+                  alt=""
+                />
+              </div>
+              <h4 className="text-center my-2 font-semibold text-2xl">
+                Mansi Sharma
+              </h4>
+              <h5 className="text-center text-xl">Media Outreach Head</h5>
+            </div>
+            <div className="bg-card p-8">
+              <div className="shadow1 p-1">
+                <img
+                  src="/assets/archita.webp"
+                  className=" w-[250px] rounded-sm"
+                  alt=""
+                />
+              </div>
+              <h4 className="text-center my-2 font-semibold text-2xl">
+                Archita Jain
+              </h4>
+              <h5 className="text-center text-xl">Event Head</h5>
             </div>
           </div>
         </div>
