b755b87a0943e19aac33ca3047e20b292b1cf081
Mohd Haroon <haroonsaifi2001@gmail.com>
2024-03-09 16:41:06 +0530

    GFG:Team Cards Fix

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -8,76 +8,271 @@ function Team() {
       <div>
         <div className="mx-auto text-white max-w-screen-2xl px-[5vw] py-8">
           <div className="flex justify-center">
-            <h1 className="font-semibold text-3xl rounded-full px-5 py-3 bg-[#9500F0]">
+            <h1 className="font-semibold text-4xl rounded-full px-5 py-3">
               CORE TEAM
             </h1>
           </div>
           <div className="flex gap-8 justify-center items-center flex-wrap mt-8">
-            <div className="bg-card p-8">
-              <div className="shadow1 ">
-                <img
-                  src="/assets/anushka.webp"
-                  className=" w-[250px] hover:translate-x-1 duration-500 hover:-translate-y-1 f1 rounded-sm"
-                  alt=""
-                />
-              </div>
+            <div className="bg-card  rounded-xl overflow-hidden p-8">
+              <img
+                src="/assets/anushka.webp"
+                className=" w-[250px] hover:translate-x-1 duration-500 hover:-translate-y-1 f1 rounded-full"
+                alt=""
+              />
               <h4 className="text-center my-2 font-semibold text-2xl">
                 Anushka Gupta
               </h4>
               <h5 className="text-center text-xl">President</h5>
-            </div>
-            <div className="bg-card p-8">
-              <div className="shadow1 ">
-                <img
-                  src="/assets/satyam.webp"
-                  className=" w-[250px] hover:translate-x-1 duration-500 hover:-translate-y-1 f1 rounded-sm"
-                  alt=""
-                />
+              <div className="flex flex-wrap justify-center items-center gap-4 text-white">
+                <a
+                  href="mailto:codechef.adgitm@adgitmdelhi.ac.in"
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                >
+                  <i className="fa-solid fa-envelope"></i>
+                </a>
+                <a
+                  href="https://www.linkedin.com/company/codechefadgitm/"
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                >
+                  <i className="fa-brands fa-linkedin"></i>
+                </a>
+                <a
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                  href="https://www.youtube.com/@CodeChefADGITMChapter"
+                >
+                  <i className="fa-brands fa-instagram"></i>
+                </a>
+                <a
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                  href="https://discord.com/invite/a5Sbt5UJEw"
+                >
+                  <i className="fa-brands fa-github"></i>
+                </a>
               </div>
+            </div>
+            <div className="bg-card rounded-xl overflow-hidden  p-8">
+              <img
+                src="/assets/satyam.webp"
+                className=" w-[250px] hover:translate-x-1 duration-500 hover:-translate-y-1 f1 rounded-full"
+                alt=""
+              />
               <h4 className="text-center my-2 font-semibold text-2xl">
                 Satyam Saini
               </h4>
               <h5 className="text-center text-xl">Voice President</h5>
-            </div>
-            <div className="bg-card p-8">
-              <div className="shadow1 ">
-                <img
-                  className=" w-[250px] hover:translate-x-1 duration-500 hover:-translate-y-1 f1 rounded-sm"
-                  src="/assets/hardik.webp"
-                  alt=""
-                />
+              <div className="flex flex-wrap justify-center items-center gap-4 text-white">
+                <a
+                  href="mailto:codechef.adgitm@adgitmdelhi.ac.in"
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                >
+                  <i className="fa-solid fa-envelope"></i>
+                </a>
+                <a
+                  href="https://www.linkedin.com/company/codechefadgitm/"
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                >
+                  <i className="fa-brands fa-linkedin"></i>
+                </a>
+                <a
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                  href="https://www.youtube.com/@CodeChefADGITMChapter"
+                >
+                  <i className="fa-brands fa-instagram"></i>
+                </a>
+                <a
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                  href="https://discord.com/invite/a5Sbt5UJEw"
+                >
+                  <i className="fa-brands fa-github"></i>
+                </a>
               </div>
+            </div>
+            <div className="bg-card  rounded-xl overflow-hidden p-8">
+              <img
+                className=" w-[250px] hover:translate-x-1 duration-500 hover:-translate-y-1 f1 rounded-full"
+                src="/assets/hardik.webp"
+                alt=""
+              />
               <h4 className="text-center my-2 font-semibold text-2xl">
                 Hardik Sharma
               </h4>
               <h5 className="text-center text-xl">General Secrectary</h5>
-            </div>
-            <div className="bg-card p-8">
-              <div className="shadow1 ">
-                <img
-                  src="/assets/mansi.webp"
-                  className=" w-[250px] hover:translate-x-1 duration-500 hover:-translate-y-1 f1 rounded-sm"
-                  alt=""
-                />
+              <div className="flex flex-wrap justify-center items-center gap-4 text-white">
+                <a
+                  href="mailto:codechef.adgitm@adgitmdelhi.ac.in"
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                >
+                  <i className="fa-solid fa-envelope"></i>
+                </a>
+                <a
+                  href="https://www.linkedin.com/company/codechefadgitm/"
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                >
+                  <i className="fa-brands fa-linkedin"></i>
+                </a>
+                <a
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                  href="https://www.youtube.com/@CodeChefADGITMChapter"
+                >
+                  <i className="fa-brands fa-instagram"></i>
+                </a>
+                <a
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                  href="https://discord.com/invite/a5Sbt5UJEw"
+                >
+                  <i className="fa-brands fa-github"></i>
+                </a>
               </div>
+            </div>
+            <div className="bg-card  rounded-xl overflow-hidden p-8">
+              <img
+                src="/assets/mansi.webp"
+                className=" w-[250px] hover:translate-x-1 duration-500 hover:-translate-y-1 f1 rounded-full"
+                alt=""
+              />
               <h4 className="text-center my-2 font-semibold text-2xl">
                 Mansi Sharma
               </h4>
               <h5 className="text-center text-xl">Media Outreach Head</h5>
-            </div>
-            <div className="bg-card p-8">
-              <div className="shadow1 ">
-                <img
-                  src="/assets/archita.webp"
-                  className=" w-[250px] hover:translate-x-1 duration-500 hover:-translate-y-1 f1 rounded-sm"
-                  alt=""
-                />
+              <div className="flex flex-wrap justify-center items-center gap-4 text-white">
+                <a
+                  href="mailto:codechef.adgitm@adgitmdelhi.ac.in"
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                >
+                  <i className="fa-solid fa-envelope"></i>
+                </a>
+                <a
+                  href="https://www.linkedin.com/company/codechefadgitm/"
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                >
+                  <i className="fa-brands fa-linkedin"></i>
+                </a>
+                <a
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                  href="https://www.youtube.com/@CodeChefADGITMChapter"
+                >
+                  <i className="fa-brands fa-instagram"></i>
+                </a>
+                <a
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                  href="https://discord.com/invite/a5Sbt5UJEw"
+                >
+                  <i className="fa-brands fa-github"></i>
+                </a>
               </div>
+            </div>
+            <div className="bg-card rounded-xl overflow-hidden  p-8">
+              <img
+                src="/assets/archita.webp"
+                className=" w-[250px] hover:translate-x-1 duration-500 hover:-translate-y-1 f1 rounded-full"
+                alt=""
+              />
               <h4 className="text-center my-2 font-semibold text-2xl">
                 Archita Jain
               </h4>
               <h5 className="text-center text-xl">Event Head</h5>
+              <div className="flex flex-wrap justify-center items-center gap-4 text-white">
+                <a
+                  href="mailto:codechef.adgitm@adgitmdelhi.ac.in"
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                >
+                  <i className="fa-solid fa-envelope"></i>
+                </a>
+                <a
+                  href="https://www.linkedin.com/company/codechefadgitm/"
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                >
+                  <i className="fa-brands fa-linkedin"></i>
+                </a>
+                <a
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                  href="https://www.youtube.com/@CodeChefADGITMChapter"
+                >
+                  <i className="fa-brands fa-instagram"></i>
+                </a>
+                <a
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                  href="https://discord.com/invite/a5Sbt5UJEw"
+                >
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </div>
             </div>
+            <div className="bg-card rounded-xl overflow-hidden  p-8">
+              <img
+                src="https://placehold.it/500"
+                className=" w-[250px] hover:translate-x-1 duration-500 hover:-translate-y-1 f1 rounded-full"
+                alt=""
+              />
+              <h4 className="text-center my-2 font-semibold text-2xl">
+                Dhurv Gera
+              </h4>
+              <h5 className="text-center text-xl">Tech Head</h5>
+              <div className="flex flex-wrap justify-center items-center gap-4 text-white">
+                <a
+                  href="mailto:codechef.adgitm@adgitmdelhi.ac.in"
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                >
+                  <i className="fa-solid fa-envelope"></i>
+                </a>
+                <a
+                  href="https://www.linkedin.com/company/codechefadgitm/"
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                >
+                  <i className="fa-brands fa-linkedin"></i>
+                </a>
+                <a
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                  href="https://www.youtube.com/@CodeChefADGITMChapter"
+                >
+                  <i className="fa-brands fa-instagram"></i>
+                </a>
+                <a
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                  href="https://discord.com/invite/a5Sbt5UJEw"
+                >
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </div>
+            </div>
+<div className="bg-card rounded-xl overflow-hidden  p-8">
+              <img
+                src="https://placehold.it/500"
+                className=" w-[250px] hover:translate-x-1 duration-500 hover:-translate-y-1 f1 rounded-full"
+                alt=""
+              />
+              <h4 className="text-center my-2 font-semibold text-2xl">
+                Anish Huzra
+              </h4>
+              <h5 className="text-center text-xl">Tech Co-Lead</h5>
+              <div className="flex flex-wrap justify-center items-center gap-4 text-white">
+                <a
+                  href="mailto:codechef.adgitm@adgitmdelhi.ac.in"
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                >
+                  <i className="fa-solid fa-envelope"></i>
+                </a>
+                <a
+                  href="https://www.linkedin.com/company/codechefadgitm/"
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                >
+                  <i className="fa-brands fa-linkedin"></i>
+                </a>
+                <a
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                  href="https://www.youtube.com/@CodeChefADGITMChapter"
+                >
+                  <i className="fa-brands fa-instagram"></i>
+                </a>
+                <a
+                  className="hover:text-[#4A0291] hover:scale-[1.2] duration-300 ease-out"
+                  href="https://discord.com/invite/a5Sbt5UJEw"
+                >
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
